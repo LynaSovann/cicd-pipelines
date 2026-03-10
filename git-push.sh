fe9c704571802e5c72d379e529407963d88f7db4
LynaSovann <sovannlyna2004@gmail.com>
2026-03-10 20:19:51 +0700

    Updated by Lyna

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Stage all changes
 git add .
 
-# Commit with the provided message
 git commit -m "Updated by Lyna"
 
-# Push to the current branch
 git push origin springboot-ecs-cicd-pipeline
 
 echo "✅ Code pushed successfully!"
